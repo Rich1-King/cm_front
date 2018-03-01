5438ccfed0651b30d679ca9222127f354ec12dda
孙崇 <rich1@sunchongdeMacBook-Pro.local>
2018-03-01 14:37:35 +0800

    .gitignore

--- src/resources/js/controllers/main.controller.js
+++ src/resources/js/controllers/main.controller.js
@@ -20,5 +20,19 @@ function controller($state) {
       var avatar = document.getElementById('avatar');
       avatar.style.visibility = 'hidden';
     }
+    vm.move_down = function(){
+      var id1 = document.getElementById("id1");
+      var div1 = document.getElementById("div1");
+      if("none" == div1.getElementsByTagName("ul")[0].style.display){
+        div1.getElementsByTagName("ul")[0].style.display="block";
+        id1.style.height = id1.offsetHeight+div1.getElementsByTagName("ul")[0].offsetHeight;
+        div1.getElementsByTagName("img")[0].src="resources/img/xia.jpg";
+      }else{
+        id1.style.height = id1.offsetHeight-div1.getElementsByTagName("ul")[0].offsetHeight;
+        div1.getElementsByTagName("ul")[0].style.display="none";
+        div1.getElementsByTagName("img")[0].src="resources/img/you.png";
+      }
+    
+    }
 }
 })();
