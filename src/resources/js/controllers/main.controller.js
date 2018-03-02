443341218a73cdf0a9b4249bc48d730396fd3a9b
孙崇 <rich1@sunchongdeMacBook-Pro.local>
2018-03-02 11:05:27 +0800

    修改动画

--- src/resources/js/controllers/main.controller.js
+++ src/resources/js/controllers/main.controller.js
@@ -20,23 +20,30 @@ function controller($state) {
       var avatar = document.getElementById('avatar');
       avatar.style.visibility = 'hidden';
     }
+
     vm.move_down = function(){
       var id1 = document.getElementById("id1");
       var div1 = document.getElementById("div1");
-      if("none" == div1.getElementsByTagName("ul")[0].style.display){
-        div1.getElementsByTagName("ul")[0].style.display="block";
-        id1.style.height = id1.offsetHeight+div1.getElementsByTagName("ul")[0].offsetHeight;
+      if(0 == id1.style.marginBottom || "0px" == id1.style.marginBottom || "undefine" == id1.style.marginBottom){
+        id1.style.marginBottom = div1.getElementsByTagName("ul")[0].offsetHeight;
         div1.getElementsByTagName("img")[0].src="resources/img/xia.jpg";
+        id1.style.transition = "margin-bottom 1s linear";
+        div1.getElementsByTagName("ul")[0].style.opacity = 1;
+        div1.getElementsByTagName("ul")[0].style.transition = "all 1s linear";
       }else{
-        id1.style.height = id1.offsetHeight-div1.getElementsByTagName("ul")[0].offsetHeight;
-        div1.getElementsByTagName("ul")[0].style.display="none";
+        id1.style.marginBottom = 0;
         div1.getElementsByTagName("img")[0].src="resources/img/you.png";
+        id1.style.transition = "margin-bottom 1s linear";
+        div1.getElementsByTagName("ul")[0].style.opacity = 0;
+        div1.getElementsByTagName("ul")[0].style.transition = "all 1s linear";
+
       }
-    
     }
+
     vm.mouse_enter = function(event){
       $(event.currentTarget).children()[0].style.display="block";
     }
+
     vm.mouse_leave = function(event){
       $(event.currentTarget).children()[0].style.display="none";
     }
